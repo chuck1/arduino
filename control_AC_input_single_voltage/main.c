e36a59816c7acb0bed504bba95d80a17c0d520ac
Charles Rymal <chuck1@users.noreply.github.com>
2018-09-28 11:39:04 -0700

    Create main.c

---
+++ control_AC_input_single_voltage/main.c
@@ -0,0 +1,11 @@
+
+
+/*
+A PID controller with the output controlling the voltage in an AC circuit using a triac and
+the input being a single voltage reading.
+The target input voltage will be hard coded for now in order to keep the electronics simple
+
+See the dimmer project to see how to control the triac.
+
+*/
+
